e5079e5d37eef62f1cb6fbacf43b9795893a067a
manish kumar <61488454+manish31011@users.noreply.github.com>
2024-08-31 12:27:23 +0100

    Add files via upload

---
+++ settings.php
@@ -0,0 +1,11 @@
+<?php
+
+	//FCM Notification
+    $fcmServerKey = 'z3SpWEcdjSnlUqRMqhGB9Xxt2jP4557TjHN1Jj9uLc3BRu2aRJ0k-2GXBu44YzhgiSHsKv62lMNROA';
+    $fcmNotificationTopic = 'blogger_news_app_topic';
+
+	//OneSignal Notification
+    $oneSignalAppId = '4230c927-6411-4eda-8151-a083486d0ee9';
+    $oneSignalRestApiKey = 'MjU0NTNhN2YtOWY2OS00MWY5LWE3OGYtYzAzNDc2N2E0MGRi';
+
+?>
